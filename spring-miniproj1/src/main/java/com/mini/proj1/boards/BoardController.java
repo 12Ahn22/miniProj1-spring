5cddcc5ce5a565ee73a232bbaa9a01a2fd5dfc14
12Ahn22 <12ayo22@gmail.com>
2024-04-07 10:58:18 +0900

    [feat] board 작업물 가져오기

---
+++ spring-miniproj1/src/main/java/com/mini/proj1/boards/BoardController.java
@@ -0,0 +1,158 @@
+package com.mini.proj1.boards;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+public class BoardController {
+	BoardService boardService = new BoardService();
+
+//	public String list(HttpServletRequest request, BoardVO boardVO ) throws Exception {
+//		List<BoardVO> list = boardService.list(boardVO);
+//		HttpSession session = request.getSession();
+//		boolean isLogin = (session.getAttribute("loginMember") != null)? true : false;
+//		
+//		request.setAttribute("list", list);
+//		request.setAttribute("isLogin", isLogin);
+//		return "boardList";
+//	}
+//
+//	public String view(HttpServletRequest request, BoardVO boardVO) {
+//		// 게시글 정보
+//		BoardVO board = boardService.view(boardVO);
+//		request.setAttribute("board", board);
+//		
+//		// 수정 가능 여부
+//		HttpSession session = request.getSession();
+//		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
+//		if(loginMember != null) { // 로그인 중인 경우
+//			if(loginMember.getId().equals(board.getAuthor()) || loginMember.getId().equals("ADMIN")) { // 내가 쓴 글 혹은 어드민
+//				request.setAttribute("isLogin", true);
+//			}
+//		}
+//		return "boardView";
+//	}
+//
+//	public Map<String,Object> delete(HttpServletRequest request, BoardVO boardVO) {
+//		Map<String, Object> map = new HashMap<>();
+//		HttpSession session = request.getSession();
+//		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember"); // 로그인 한 유저
+//		
+//		// 로그인 하지않았다면
+//		if(loginMember == null) {
+//			map.put("status", 404);
+//			map.put("statusMessage", "로그인을 해야합니다.");
+//			return map;
+//		}
+//		
+//		// 현재 세션의 유저와 입력된 정보의 id가 같지 않다면 수정 불가
+//		if(!loginMember.getId().equals("ADMIN")) { // 어드민이 아닐 때만 체크
+//			if(!loginMember.getId().equals(boardVO.getAuthor())) { // 로그인 유저와 요청 유저가 다르다면
+//				// 실패
+//				map.put("status", 404);
+//				map.put("statusMessage", "잘못된 요청입니다.");
+//				return map;
+//			}
+//		}
+//		
+//		int updated = boardService.delete(boardVO);
+//		
+//		if(updated == 1) { // 성공
+//			map.put("status", 204);
+//		} else {
+//			map.put("status", 404);
+//			map.put("statusMessage", "게시글 삭제에 실패하였습니다");
+//		}
+//		
+//		return map;
+//	}
+//
+//	public String updateForm(HttpServletRequest request, BoardVO boardVO) {
+//		// 게시글 정보
+//		BoardVO board = boardService.fetchUpdateFormData(boardVO);
+//		request.setAttribute("board", board);
+//		return "boardUpdate";
+//	}
+//
+//	public Map<String, Object> update(HttpServletRequest request, BoardVO boardVO) {
+//		Map<String, Object> map = new HashMap<>();
+//		HttpSession session = request.getSession();
+//		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember"); // 로그인 한 유저
+//		
+//		// 로그인 하지않았다면
+//		if(loginMember == null) {
+//			map.put("status", 404);
+//			map.put("statusMessage", "로그인을 해야합니다.");
+//			return map;
+//		}
+//		
+//		// 현재 세션의 유저와 입력된 정보의 id가 같지 않다면 수정 불가
+//		if(!loginMember.getId().equals("ADMIN")) { // 어드민이 아닐 때만 체크
+//			if(!loginMember.getId().equals(boardVO.getAuthor())) { // 로그인 유저와 요청 유저가 다르다면
+//				// 실패
+//				map.put("status", 404);
+//				map.put("statusMessage", "잘못된 요청입니다.");
+//				return map;
+//			}
+//		}
+//		
+//		int updated = boardService.update(boardVO);
+//		
+//		if(updated == 1) { // 성공
+//			map.put("status", 204);
+//		} else {
+//			map.put("status", 404);
+//			map.put("statusMessage", "게시글 수정에 실패하였습니다");
+//		}
+//		
+//		return map;
+//	}
+//
+//	public String insertForm(HttpServletRequest request) {
+//		// 로그인 상태라면, 해당 로그인 정보를 JSP로 보내주기
+//		HttpSession session = request.getSession();
+//		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
+//		if(loginMember != null) {
+//			request.setAttribute("member", loginMember);
+//		}
+//		return "boardInsert";
+//	}
+//
+//	public Map<String, Object> insert(HttpServletRequest request, BoardVO boardVO) {
+//		Map<String, Object> map = new HashMap<>();
+//		HttpSession session = request.getSession();
+//		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember"); // 로그인 한 유저
+//		
+//		// 로그인 하지않았다면
+//		if(loginMember == null) {
+//			map.put("status", 404);
+//			map.put("statusMessage", "로그인을 해야합니다.");
+//			return map;
+//		}
+//		
+//		// 현재 세션의 유저와 입력된 정보의 id가 같지 않다면 수정 불가
+//		if(!loginMember.getId().equals("ADMIN")) { // 어드민이 아닐 때만 체크
+//			if(!loginMember.getId().equals(boardVO.getAuthor())) { // 로그인 유저와 요청 유저가 다르다면
+//				// 실패
+//				map.put("status", 404);
+//				map.put("statusMessage", "잘못된 요청입니다.");
+//				return map;
+//			}
+//		}
+//		
+//		int updated = boardService.insert(boardVO);
+//		
+//		if(updated == 1) { // 성공
+//			map.put("status", 204);
+//		} else {
+//			map.put("status", 404);
+//			map.put("statusMessage", "게시글 생성에 실패하였습니다");
+//		}
+//		
+//		return map;
+//	}
+
+}
---
+++ spring-miniproj1/src/main/java/com/mini/proj1/boards/BoardService.java
@@ -0,0 +1,94 @@
+package com.mini.proj1.boards;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BoardService {
+	BoardDAO boardDAO = new BoardDAO();
+//	
+//	public List<BoardVO> list(BoardVO boardVO) throws Exception {
+//		List<BoardVO> list = null;
+//		try {
+//			list = boardDAO.list(boardVO.getSearchKey());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw new Exception();
+//		}
+//		return list;
+//	}
+//
+//	public BoardVO view(BoardVO boardVO) {
+//		BoardVO board = null;
+//		try {
+//			// view 카운트를 증가
+//			boardDAO.increaseViewCount(boardVO.getBno());
+//			board = boardDAO.view(boardVO.getBno());
+//			BaseDAO.conn.commit();
+//		} catch (Exception e) {
+//			try {
+//				BaseDAO.conn.rollback();
+//			} catch (SQLException e1) {
+//				e1.printStackTrace();
+//			}
+//		}
+//		// board 반환
+//		return board; // 만약 얘가 없으면 어떻게 처리하지?
+//	}
+//	
+//	public BoardVO fetchUpdateFormData(BoardVO boardVO) {
+//		BoardVO board = null;
+//		try {
+//			board =  boardDAO.view(boardVO.getBno());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return board;
+//	}
+//
+//	public int delete(BoardVO boardVO) {
+//		try {
+//			boardDAO.delete(boardVO.getBno());
+//			BaseDAO.conn.commit();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			try {
+//				BaseDAO.conn.rollback();
+//			} catch (SQLException e1) {
+//				e1.printStackTrace();
+//			}
+//			return 0;
+//		}
+//		return 1;
+//	}
+//
+//	public int update(BoardVO boardVO) {
+//		try {
+//			boardDAO.update(boardVO);
+//			BaseDAO.conn.commit();
+//		} catch (Exception e) {
+//			try {
+//				BaseDAO.conn.rollback();
+//			} catch (SQLException e1) {
+//				e1.printStackTrace();
+//			}
+//			return 0;
+//		}
+//		return 1;
+//	}
+//
+//	public int insert(BoardVO boardVO) {
+//		try {
+//			boardDAO.insert(boardVO);
+//			BaseDAO.conn.commit();
+//		} catch (Exception e) {
+//			try {
+//				BaseDAO.conn.rollback();
+//			} catch (SQLException e1) {
+//				e1.printStackTrace();
+//			}
+//			return 0;
+//		}
+//		return 1;
+//	}
+
+}
